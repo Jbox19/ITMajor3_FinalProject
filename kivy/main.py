31955226f854e1194e6b7c10ed2bf3de548f761f
BUNI Revillame (jieo) <87221217+Jbox19@users.noreply.github.com>
2024-10-31 21:05:46 +0800

    Add files via upload

---
+++ kivy/main.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivy.uix.button import Button
+from kivy.lang import Builder
+
+class MyApp(MDApp):
+    pass
+
+    
+if __name__ == '__main__':
+    MyApp().run()
